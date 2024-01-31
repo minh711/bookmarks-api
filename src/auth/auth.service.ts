c31b00e69b28198c08531bf4c94ea5dffc6a78a8
minh711 <duyminhtruong711@gmail.com>
2024-01-31 11:44:48 +0700

    Update `signin` function.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -37,7 +37,29 @@ export class AuthService {
     }
   }
   
-  signin() {
-    return { msg: 'I am signing in' };
+  async signin(dto: AuthDto) {
+    // find the user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      }
+    })
+
+    // if user does not exist throw exception
+    if (!user) throw new ForbiddenException('Credentials incorrect');
+    
+    // compare password
+    const pwMatches = await argon.verify(
+      user.hash, 
+      dto.password
+    );
+
+    // if password incorrect throw exception
+    if (!pwMatches)
+      throw new ForbiddenException('Credentials incorrect');
+    
+    // send back the user
+    delete user.hash // don't show the hash field
+    return user;
   } 
 }
