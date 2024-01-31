86535f7fafa2b874dbdff89129d63cafb33987ba
minh711 <duyminhtruong711@gmail.com>
2024-01-31 10:45:52 +0700

    Update comment.

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
-    whitelist: true // prevent to receive unwanted field
+    whitelist: true // stripping out the elements that are not defined in the DTO
   }));
   await app.listen(3000);
 }
