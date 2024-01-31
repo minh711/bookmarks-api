bbafcf895293da1d932154f213cdc1f4e4dd1587
minh711 <duyminhtruong711@gmail.com>
2024-01-31 10:36:27 +0700

    DTO and Pipes.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from "@nestjs/common";
 import { User, Bookmark } from '@prisma/client'
+import { PrismaService } from "../prisma/prisma.service";
 
 @Injectable({})
 export class AuthService {
-
+  constructor(private prisma: PrismaService) {}
+  
   signup() {
     return { msg: 'I am signed up' };
   }
