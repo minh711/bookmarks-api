d6b563f213f423dc3f99b3b9d58e7ca50274bea5
minh711 <duyminhtruong711@gmail.com>
2024-01-31 11:34:00 +0700

    Catch email duplication error.
    https://github.com/prisma/prisma/issues/17945
    
    https://www.prisma.io/docs/concepts/components/prisma-client/handling-exceptions-and-errors

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,30 +1,40 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { User, Bookmark } from '@prisma/client'
 import { PrismaService } from "../prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2';
+import { Prisma } from '@prisma/client';
 
 @Injectable({})
 export class AuthService {
   constructor(private prisma: PrismaService) {}
   
   async signup(dto: AuthDto) {
-    // generate the password hash
-    const hash = await argon.hash(dto.password);
-    // save the new user in the database
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-      select: {
-        id: true,
-        email: true,
-        createdAt: true
+    try {
+      // generate the password hash
+      const hash = await argon.hash(dto.password);
+      // save the new user in the database
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+        select: {
+          id: true,
+          email: true,
+          createdAt: true
+        }
+      })
+      // return the saved user
+      return user;
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        } 
       }
-    })
-    // return the saved user
-    return user;
+      throw error;
+    }
   }
   
   signin() {
